70272a3fa1fce15f14975980a814e20e7dc3802d
majithabhanus <majithabhnaus@gmail.com>
2026-02-18 12:39:18 +0530

    test-3

---
+++ INSTANCE-CREATION/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y wget gnupg software-properties-common
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://packages.adoptium.net/artifactory/deb
+sudo apt update
+sudo apt install temurin-21-jdk -y
+/usr/bin/java --version
+
+#install jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+sudo systemctl restart jenkins
+docker run -d --name sonar -p 9000:9000 sonarqube:community
+
+
+
+
+
+
+
