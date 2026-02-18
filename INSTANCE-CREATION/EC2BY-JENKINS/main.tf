9f73af0330835c9fac5db1d84e40cdf5884f09a5
majithabhanus <majithabhnaus@gmail.com>
2026-02-18 14:42:03 +0530

    eee

--- INSTANCE-CREATION/EC2BY-JENKINS/main.tf
+++ INSTANCE-CREATION/EC2BY-JENKINS/main.tf
@@ -1,14 +1,40 @@
 provider "aws" {
-  region = "us-east-1"   # Mumbai region (change if needed)
+  region = "us-east-1"   # Change if needed
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0b6c6ebed2801a5cb"   # Amazon Linux 2 (ap-south-1)
-  instance_type = "t2.micro"
+resource "aws_security_group" "ec2_sg" {
+  name = "jenkins-ec2-sg"
 
-  key_name = "rrr"   # replace with your keypair
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-0b6c6ebed2801a5cb"   # Amazon Linux (Mumbai)
+  instance_type = "t2.micro"
+  key_name      = "rrr"      # Change this!
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = "Jenkins-EC2."
+    Name = "EC2-Created-By-Jenkins"
   }
 }
